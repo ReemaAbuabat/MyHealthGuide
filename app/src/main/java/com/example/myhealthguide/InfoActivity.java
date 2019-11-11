767685a5f5f2eea177410552a485707127260c7e
Reema Abuabat <ReemaAbuabat@gmail.com>
2019-11-11 09:22:53 +0300

    Revert "final sprint 4"

--- app/src/main/java/com/example/myhealthguide/InfoActivity.java
+++ app/src/main/java/com/example/myhealthguide/InfoActivity.java
@@ -34,14 +34,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.varunest.sparkbutton.SparkButton;
+import com.varunest.sparkbutton.SparkEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
-
 public class InfoActivity extends AppCompatActivity {
-<<<<<<< HEAD
-   
-=======
     private RecyclerView recyclerView;
     private RowAdapter adapter;
     private List<Row> albumList;
@@ -50,7 +48,9 @@ public class InfoActivity extends AppCompatActivity {
     private boolean check ;
     private FirebaseUser user;
     private ProgressDialog progressDialog;
-    ImageView healthMin, share, favourit;
+    ImageView healthMin, share;
+    SparkButton sparkButton;
+
 
     ArrayList<Favourite> favouriteArrayList = new ArrayList<>();
 
@@ -64,14 +64,14 @@ public class InfoActivity extends AppCompatActivity {
         initCollapsingToolbar();
         getList();
 
-        healthMin = findViewById(R.id.healthMinstry);
-        share=findViewById(R.id.shareBTN);
-        favourit = findViewById(R.id.favBTN);
-        favourit.setOnClickListener(new View.OnClickListener() {
+
+
+        sparkButton = findViewById(R.id.test);
+
+        sparkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                if(vlidate()){
+                if (vlidate()) {
                     user = FirebaseAuth.getInstance().getCurrentUser();
                     String userId = user.getUid();
 
@@ -84,15 +84,61 @@ public class InfoActivity extends AppCompatActivity {
                     Favourite favourite = new Favourite(id, albumList.get(postion).getdName());
                     favouriteReference.child(id).setValue(favourite);
                     Dialog("added to your favourite !");
+                    sparkButton.setChecked(true);
 
 
-                }else{
+                } else {
                     wrongInfoDialog("its already in your favourite list");
+                    sparkButton.setChecked(true);
+
+                }
+            }
+        });
+        sparkButton.setEventListener(new SparkEventListener() {
+            @Override
+            public void onEvent(ImageView button, boolean buttonState) {
+                for (Favourite favourite : favouriteArrayList) {
+                    if (name.equals(favourite.getFavName())) {
+                        sparkButton.setChecked(true);
+                        break;
+                    }
+                }
+
+            }
+
+            @Override
+            public void onEventAnimationEnd(ImageView button, boolean buttonState) {
+                for (Favourite favourite : favouriteArrayList) {
+                    if (name.equals(favourite.getFavName())) {
+                        sparkButton.setChecked(true);
+                        break;
+                    }
+                }
+
+            }
+
+            @Override
+            public void onEventAnimationStart(ImageView button, boolean buttonState) {
+
+                for (Favourite favourite : favouriteArrayList) {
+                    if (name.equals(favourite.getFavName())) {
+                        sparkButton.setChecked(true);
+                        break;
+                    }
                 }
 
 
             }
         });
+
+
+
+        sparkButton.playAnimation();
+
+
+        healthMin = findViewById(R.id.healthMinstry);
+        share=findViewById(R.id.shareBTN);
+
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         healthBtn=(Button) findViewById(R.id.healthBtn);
         albumList = new ArrayList<>();
@@ -108,7 +154,7 @@ public class InfoActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent myIntent = new Intent(Intent.ACTION_SEND);
                 myIntent.setType("text/plain");
-                String shareBody = "/n allowed food:"+allowedFood+"/n Not allowed food: "+notAllowedFood;
+                String shareBody = "disease:"+name+"\n allowed food: \n"+allowedFood+"\n Not allowed food: \n"+notAllowedFood;
                 myIntent.putExtra(Intent.EXTRA_SUBJECT, name);
                 myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
                 startActivity(Intent.createChooser(myIntent, "Share using"));
@@ -379,5 +425,4 @@ public class InfoActivity extends AppCompatActivity {
 
     }//end wrongInfoDialog()
 
->>>>>>> parent of ca3fec6... final sprint 4
 }
